d595410e03513b8a3dda454c1442dd468cc48be2
John Lim <47646455+johnlim612@users.noreply.github.com>
2024-09-30 15:12:50 -0700

    file manager reformat

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
-    
+
     # Register blueprints or import routes
     from .routes import main
+
     app.register_blueprint(main)
-    
+
     return app
